573016cd12ad1c82d04113a44efb8b455117255c
Sean Hong <62536313+seanhong7777@users.noreply.github.com>
2020-12-30 07:06:07 +0900

    Add files via upload

---
+++ Nadocoding.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(void)
+
+{
+	for (int i = 1; i <= 30; i++)
+	{
+		if (i >= 6 && i <= 10)
+		{
+			if (i = 7)
+			{
+				printf_s("%d번 학생은 결석입니다\n", if)
+			}
+			printf_s("%d번 학생은 조별 발표 준비를 하세요\n", i)
+		}
+	}
+
+	return 0;
+}
+
